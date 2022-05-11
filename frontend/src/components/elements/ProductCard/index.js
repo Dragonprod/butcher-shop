ae27f14d84956bede4df339eca4de77d9ffe1141
maxcore25 <mkorobov2002@mail.ru>
2022-05-12 00:39:37 +0300

    Product Card Dynamic Data Added

--- frontend/src/components/elements/ProductCard/index.js
+++ frontend/src/components/elements/ProductCard/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import styles from './ProductCard.module.scss';
 
 export default function ProductCard({ productCard }) {
-  return <div>ProductCard</div>;
+  return (
+    <div className={styles.card}>
+      <div className={styles.photoContainer}>
+        <img src={productCard.photoPath} alt={productCard.name} />
+      </div>
+      <div className={styles.contentContainer}>
+        <h3>{productCard.name}</h3>
+        <div>
+          {Array.from(Array(productCard.stars), (e, i) => (
+            <span className={styles.star} key={i}>
+              {i}
+            </span>
+          ))}
+        </div>
+        <p>{productCard.price}</p>
+      </div>
+      <button className={styles.contentContainer}>Добавить в корзину</button>
+    </div>
+  );
 }
--- frontend/src/components/modules/ProductCards/index.js
+++ frontend/src/components/modules/ProductCards/index.js
@@ -4,10 +4,10 @@ import ProductCard from '../../elements/ProductCard';
 
 export default function ProductCards() {
   let productCards = [
-    { id: 0, name: 'Бутер', photo: '', stars: 1, price: 100 },
-    { id: 1, name: 'Бургер', photo: '', stars: 2, price: 120 },
-    { id: 2, name: 'Стейк говядины', photo: '', stars: 3, price: 130 },
-    { id: 3, name: 'Бараньи рёбра', photo: '', stars: 4, price: 140 },
+    { id: 0, name: 'Бутер', photoPath: '', stars: 1, price: 100 },
+    { id: 1, name: 'Бургер', photoPath: '', stars: 2, price: 120 },
+    { id: 2, name: 'Стейк говядины', photoPath: '', stars: 3, price: 130 },
+    { id: 3, name: 'Бараньи рёбра', photoPath: '', stars: 4, price: 140 },
   ];
 
   return (
