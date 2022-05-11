e288c40c4bce8044d07b6917161dcc1d707f2796
maxcore25 <mkorobov2002@mail.ru>
2022-05-11 23:24:56 +0300

    Banner Created

---
+++ frontend/src/components/modules/Banner/index.js
@@ -0,0 +1,22 @@
+import styles from './Banner.module.scss';
+import React from 'react';
+
+export default function Banner() {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.textContainer}>
+        <h1>
+          Супер классная
+          <br />
+          мясная лавка
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/default/catalog/CatalogPage.js
+++ frontend/src/pages/default/catalog/CatalogPage.js
@@ -1,9 +1,12 @@
+import Banner from '../../../components/modules/Banner';
 import Header from '../../../components/modules/Header';
 
 export default function CatalogPage() {
   return (
     <>
       <Header />
+
+      <Banner />
     </>
   );
 }
