f2a543120009665cef6e246d9335dde3d33a5061
maxcore25 <mkorobov2002@mail.ru>
2022-05-12 10:04:14 +0300

    Example Product Image Added

--- frontend/src/components/elements/ProductCard/index.js
+++ frontend/src/components/elements/ProductCard/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './ProductCard.module.scss';
+import img1 from '../../../assets/images/1.png';
 
 export default function ProductCard({ productCard }) {
+  // TODO: Dynamic Images
   return (
     <div className={styles.card}>
       <div className={styles.photoContainer}>
-        <img src={productCard.photoPath} alt={productCard.name} />
+        {/* <img src={productCard.photoPath} alt={productCard.name} /> */}
+        <img src={img1} alt={productCard.name} />
       </div>
       <div className={styles.contentContainer}>
         <h3>{productCard.name}</h3>
--- frontend/src/components/modules/ProductCards/index.js
+++ frontend/src/components/modules/ProductCards/index.js
@@ -4,10 +4,34 @@ import ProductCard from '../../elements/ProductCard';
 
 export default function ProductCards() {
   let productCards = [
-    { id: 0, name: 'Бутер', photoPath: '', stars: 1, price: 100 },
-    { id: 1, name: 'Бургер', photoPath: '', stars: 2, price: 120 },
-    { id: 2, name: 'Стейк говядины', photoPath: '', stars: 3, price: 130 },
-    { id: 3, name: 'Бараньи рёбра', photoPath: '', stars: 4, price: 140 },
+    {
+      id: 0,
+      name: 'Бутер',
+      photoPath: '../../../assets/images/1.png',
+      stars: 1,
+      price: 100,
+    },
+    {
+      id: 1,
+      name: 'Бургер',
+      photoPath: '../../../assets/images/1.png',
+      stars: 2,
+      price: 120,
+    },
+    {
+      id: 2,
+      name: 'Стейк говядины',
+      photoPath: '../../../assets/images/1.png',
+      stars: 3,
+      price: 130,
+    },
+    {
+      id: 3,
+      name: 'Бараньи рёбра',
+      photoPath: '../../../assets/images/1.png',
+      stars: 4,
+      price: 140,
+    },
   ];
 
   return (
