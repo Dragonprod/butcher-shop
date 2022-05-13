e773d1d33c42db9efdf403c54645ee4ca4e9eecb
maxcore25 <mkorobov2002@mail.ru>
2022-05-13 12:29:54 +0300

    Icons Deleted

--- frontend/src/components/modules/CategoriesList/index.js
+++ frontend/src/components/modules/CategoriesList/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ListSubheader from '@mui/material/ListSubheader';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import StarBorder from '@mui/icons-material/StarBorder';
@@ -30,36 +26,24 @@ export default function CategoriesList() {
           </ListSubheader>
         }>
         <ListItemButton>
-          <ListItemIcon>
-            <SendIcon />
-          </ListItemIcon>
           <ListItemText primary='Бургеры' />
         </ListItemButton>
         <ListItemButton>
-          <ListItemIcon>
-            <DraftsIcon />
-          </ListItemIcon>
           <ListItemText primary='Котлеты' />
         </ListItemButton>
         <ListItemButton onClick={handleClick}>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
+          {/* <ListItemIcon>
+            <StarBorder />
+          </ListItemIcon> */}
           <ListItemText primary='Стейки' />
           {open ? <ExpandLess /> : <ExpandMore />}
         </ListItemButton>
         <Collapse in={open} timeout='auto' unmountOnExit>
           <List component='div' disablePadding>
             <ListItemButton sx={{ pl: 4 }}>
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
               <ListItemText primary='Говядина' />
             </ListItemButton>
             <ListItemButton sx={{ pl: 4 }}>
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
               <ListItemText primary='Баранина' />
             </ListItemButton>
           </List>
