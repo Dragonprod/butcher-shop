7ffb458717e9e9d64158cc5725e973f04d906b77
maxcore25 <mkorobov2002@mail.ru>
2022-05-13 19:35:31 +0300

    CartPage Stylized

--- frontend/src/pages/user/cart/CartPage.js
+++ frontend/src/pages/user/cart/CartPage.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import Footer from '../../../components/modules/Footer';
+import Header from '../../../components/modules/Header';
 import styles from './CartPage.module.scss';
 
 export default function CartPage() {
-  return <div className={styles.mainGrid}>CartPage</div>;
+  return (
+    <>
+      <Header />
+      <section className={styles.mainContainer}></section>
+      <Footer />
+    </>
+  );
 }
