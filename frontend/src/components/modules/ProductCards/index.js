892b729331112f73e7ea891849f6f2f42dfcbee1
maxcore25 <mkorobov2002@mail.ru>
2022-05-14 01:16:08 +0300

    CatalogPage Remade

--- frontend/src/components/modules/ProductCards/index.js
+++ frontend/src/components/modules/ProductCards/index.js
@@ -1,46 +1,108 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './ProductCards.module.scss';
 import ProductCard from '../../elements/ProductCard';
 
 export default function ProductCards() {
-  let defaultProductCards = [
+  let defaultCategories = [
     {
       id: 0,
-      name: 'Бутер',
-      photoPath: '../../../assets/images/1.png',
-      stars: 1,
-      price: 100,
+      name: 'Бургеры',
+      products: [
+        {
+          id: 0,
+          name: 'Чизбургер',
+          photoPath: '../../../assets/images/1.png',
+          stars: 1,
+          price: 50,
+        },
+        {
+          id: 1,
+          name: 'Гамбургер',
+          photoPath: '../../../assets/images/1.png',
+          stars: 2,
+          price: 50,
+        },
+        {
+          id: 2,
+          name: 'Чикен бургер',
+          photoPath: '../../../assets/images/1.png',
+          stars: 3,
+          price: 50,
+        },
+        {
+          id: 3,
+          name: 'Биг Мак',
+          photoPath: '../../../assets/images/1.png',
+          stars: 4,
+          price: 300,
+        },
+      ],
     },
     {
       id: 1,
-      name: 'Бургер',
-      photoPath: '../../../assets/images/1.png',
-      stars: 2,
-      price: 120,
+      name: 'Котлеты',
+      products: [
+        {
+          id: 4,
+          name: 'Бургерные котлеты',
+          photoPath: '../../../assets/images/1.png',
+          stars: 2,
+          price: 120,
+        },
+        {
+          id: 5,
+          name: 'Домашние котлеты',
+          photoPath: '../../../assets/images/1.png',
+          stars: 3,
+          price: 130,
+        },
+        {
+          id: 6,
+          name: 'Рыбные котлеты',
+          photoPath: '../../../assets/images/1.png',
+          stars: 4,
+          price: 140,
+        },
+      ],
     },
     {
       id: 2,
-      name: 'Стейк говядины',
-      photoPath: '../../../assets/images/1.png',
-      stars: 3,
-      price: 130,
-    },
-    {
-      id: 3,
-      name: 'Бараньи рёбра',
-      photoPath: '../../../assets/images/1.png',
-      stars: 4,
-      price: 140,
+      name: 'Стейки',
+      products: [
+        {
+          id: 7,
+          name: 'Стейк из говядины',
+          photoPath: '../../../assets/images/1.png',
+          stars: 1,
+          price: 100,
+        },
+        {
+          id: 8,
+          name: 'Стейк из баранины',
+          photoPath: '../../../assets/images/1.png',
+          stars: 2,
+          price: 120,
+        },
+      ],
     },
   ];
-  const [productCards, setProductCards] = useState(defaultProductCards);
+  const [categories, setCategories] = useState(defaultCategories);
 
-  // TODO: Fetch Data
+  useEffect(() => {
+    // TODO: Fetch data
+  }, []);
 
   return (
-    <div className={styles.grid}>
-      {productCards.map(productCard => (
-        <ProductCard key={productCard.id} productCard={productCard} />
+    <div className={styles.test}>
+      {categories.map(category => (
+        <div key={category.id} className={styles.categoryContainer}>
+          <h2>{category.name}</h2>
+          <div className={styles.productsGrid}>
+            {category.products.map(product => (
+              <ProductCard key={product.id} productCard={product} />
+            ))}
+          </div>
+        </div>
       ))}
     </div>
   );
--- frontend/src/pages/default/catalog/CatalogPage.js
+++ frontend/src/pages/default/catalog/CatalogPage.js
@@ -1,21 +1,16 @@
 import Banner from '../../../components/modules/Banner';
-import CategoriesList from '../../../components/modules/CategoriesList';
 import Footer from '../../../components/modules/Footer';
 import Header from '../../../components/modules/Header';
 import ProductCards from '../../../components/modules/ProductCards';
 import styles from './CatalogPage.module.scss';
 
 export default function CatalogPage() {
-  // TODO: Remake Categories Template
   return (
     <>
       <Header />
       <main className={styles.mainGrid}>
         <Banner />
-        <div className={styles.goodsGrid}>
-          <CategoriesList />
-          <ProductCards />
-        </div>
+        <ProductCards />
       </main>
       <Footer />
     </>
