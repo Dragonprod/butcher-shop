6b37373caff7417dc49c91cc41fa79fe3d33098e
Dragonprod <artyom2002-98@mail.ru>
2022-05-13 18:03:59 +0300

    add scss file for `Order` and `Users` page

--- frontend/src/components/modules/Footer/index.js
+++ frontend/src/components/modules/Footer/index.js
@@ -4,7 +4,7 @@ import styles from './Footer.module.scss';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      {new Date().getFullYear()}© Butcher Shop. ALL RIGHTS RESERVED
+      {new Date().getFullYear()} © Butcher Shop. ALL RIGHTS RESERVED
     </footer>
   );
 }
