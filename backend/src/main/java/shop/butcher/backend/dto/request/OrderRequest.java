ececb1e78703219737181f4a3bf161ae8c3e89c0
Dragonprod <artyom2002-98@mail.ru>
2022-05-15 18:54:27 +0300

    linter

--- backend/src/main/java/shop/butcher/backend/dto/request/OrderRequest.java
+++ backend/src/main/java/shop/butcher/backend/dto/request/OrderRequest.java
@@ -1,7 +1,5 @@
 package shop.butcher.backend.dto.request;
 
-import shop.butcher.backend.entity.Product;
-
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.Map;
