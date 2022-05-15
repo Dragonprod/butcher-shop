5759c906233995c4c0f6e3a4c155d3d38bf01fdd
maxcore25 <mkorobov2002@mail.ru>
2022-05-15 22:13:32 +0300

    Order Table Header Stylized

--- frontend/src/components/modules/OrdersTableBlock/index.js
+++ frontend/src/components/modules/OrdersTableBlock/index.js
@@ -1,10 +1,11 @@
-import { Avatar, Button, TextField } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UIContext } from '../../../context/UIContext';
 import styles from './OrdersTableBlock.module.scss';
 
 export default function OrdersTableBlock() {
   const [login, setLogin] = useState('user@mail.ru');
   const [password, setPassword] = useState('123456');
+  const { cartProducts } = useContext(UIContext);
 
   useEffect(() => {
     // TODO: Fetch Data
@@ -25,34 +26,18 @@ export default function OrdersTableBlock() {
   return (
     <div>
       <h2 className={styles.title}>Заказы</h2>
-      <div className={styles.block}>
-        <div className={styles.avatarContainer}></div>
-        <div className={styles.infoContainer}>
-          <form
-            className={`${styles.form}`}
-            autoComplete='off'
-            onSubmit={handleSubmit}>
-            <Button
-              className={styles.submit}
-              type='submit'
-              variant='contained'
-              sx={{
-                background: '#ff683a',
-                marginTop: 5,
-                '&.MuiButton-root.MuiButton-contained': {
-                  // width: '100%',
-                  borderRadius: '12px',
-                  padding: '16px',
-                  textTransform: 'capitalize',
-                  fontSize: '16px',
-                  lineHeight: '145%',
-                },
-              }}>
-              Редактировать
-            </Button>
-          </form>
+      <ul className={styles.table}>
+        <div className={styles.tableHeader}>
+          <h3>Фото</h3>
+          <h3>Описание</h3>
+          <h3>Цена</h3>
+          <h3>Количество</h3>
+          <h3>Сумма</h3>
         </div>
-      </div>
+        {cartProducts.map(cartItem => (
+          <div></div>
+        ))}
+      </ul>
     </div>
   );
 }
