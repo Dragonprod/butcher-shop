58b95dfb8f733985a5eaf8073b0498806476eee7
maxcore25 <mkorobov2002@mail.ru>
2022-05-15 17:14:25 +0300

    UserInfoBlock Created

---
+++ frontend/src/components/modules/UserInfoBlock/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './UserInfoBlock.module.scss';
+
+export default function UserInfoBlock() {
+  return <div className={styles.block}>UserInfoBlock</div>;
+}
--- frontend/src/pages/user/profile/ProfilePage.js
+++ frontend/src/pages/user/profile/ProfilePage.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Footer from '../../../components/modules/Footer';
 import Header from '../../../components/modules/Header';
+import UserInfoBlock from '../../../components/modules/UserInfoBlock';
 import styles from './ProfilePage.module.scss';
 
 export default function ProfilePage() {
   return (
     <>
       <Header />
-      <section className={styles.mainGrid}></section>
+      <section className={styles.mainContainer}>
+        <UserInfoBlock />
+      </section>
+      <Footer />
     </>
   );
 }
