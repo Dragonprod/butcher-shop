a2f035d92152a8345172b7abd698d58329d51f2e
maxcore25 <mkorobov2002@mail.ru>
2022-05-17 01:45:23 +0300

    ADD_CATEGORY And ADD_PRODUCT Constants Added

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,3 +1,5 @@
 export const LOGIN = 'LOGIN';
 export const REGISTER = 'REGISTER';
 export const AUTH_PAGE = 'AUTH_PAGE';
+export const ADD_CATEGORY = 'ADD_CATEGORY';
+export const ADD_PRODUCT = 'ADD_PRODUCT';
