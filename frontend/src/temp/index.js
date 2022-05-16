e3a8b81b2329575116412a44e37dde5d2e88d365
maxcore25 <mkorobov2002@mail.ru>
2022-05-16 12:08:31 +0300

    Temporary Orders Created

---
+++ frontend/src/temp/index.js
@@ -0,0 +1,66 @@
+export const defaultOrders = [
+  {
+    id: 0,
+    products: [
+      {
+        id: 0,
+        name: 'Чикен бургер',
+        photoUrl: './assets/images/1.png',
+      },
+      {
+        id: 1,
+        name: 'Гамбургер',
+        photoUrl: './assets/images/1.png',
+      },
+      {
+        id: 2,
+        name: 'Чизбургер',
+        photoUrl: './assets/images/1.png',
+      },
+    ],
+    createDate: '16.05.2002',
+    sum: 999,
+    status: false,
+  },
+  {
+    id: 1,
+    products: [
+      {
+        id: 0,
+        name: 'Стейк из баранины',
+        photoUrl: './assets/images/1.png',
+      },
+      {
+        id: 1,
+        name: 'Стейк из говядины',
+        photoUrl: './assets/images/1.png',
+      },
+    ],
+    createDate: '10.05.2002',
+    sum: 5500,
+    status: true,
+  },
+  {
+    id: 1,
+    products: [
+      {
+        id: 0,
+        name: 'Вода',
+        photoUrl: './assets/images/1.png',
+      },
+      {
+        id: 1,
+        name: 'Пиво',
+        photoUrl: './assets/images/1.png',
+      },
+      {
+        id: 2,
+        name: 'Лимонад',
+        photoUrl: './assets/images/1.png',
+      },
+    ],
+    createDate: '10.05.2002',
+    sum: 1500,
+    status: true,
+  },
+];
