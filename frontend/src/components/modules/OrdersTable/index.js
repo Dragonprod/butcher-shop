739865044c96150af845dde7b1971eacb45135ba
maxcore25 <mkorobov2002@mail.ru>
2022-05-16 20:07:38 +0300

    OrdersPage Positioning And Grid Refactored

--- frontend/src/components/modules/OrdersTable/index.js
+++ frontend/src/components/modules/OrdersTable/index.js
@@ -15,45 +15,49 @@ export default function OrdersTable() {
 
   return (
     <>
-      <h2 className={styles.title}>Незавершённые заказы</h2>
-      {uncompletedOrders.length > 0 ? (
-        <ul className={styles.table}>
-          <div className={styles.tableHeader}>
-            <h3>ID заказа</h3>
-            <h3>Пользователь</h3>
-            <h3>Список товаров</h3>
-            <h3>Дата оформления</h3>
-            <h3>Сумма</h3>
-          </div>
-          {uncompletedOrders.map(adminOrder => (
-            <AdminOrderItem key={adminOrder.id} adminOrder={adminOrder} />
-          ))}
-        </ul>
-      ) : (
-        <>
-          <h2 className={styles.emptyCartTitle}>Нет записей</h2>
-        </>
-      )}
+      <div className={styles.tableContainer}>
+        <h2 className={styles.title}>Незавершённые заказы</h2>
+        {uncompletedOrders.length > 0 ? (
+          <ul className={styles.table}>
+            <div className={styles.tableHeader}>
+              <h3>ID заказа</h3>
+              <h3>Пользователь</h3>
+              <h3>Список товаров</h3>
+              <h3>Дата оформления</h3>
+              <h3>Сумма</h3>
+            </div>
+            {uncompletedOrders.map(adminOrder => (
+              <AdminOrderItem key={adminOrder.id} adminOrder={adminOrder} />
+            ))}
+          </ul>
+        ) : (
+          <>
+            <h2 className={styles.emptyCartTitle}>Нет записей</h2>
+          </>
+        )}
+      </div>
 
-      <h2 className={styles.title}>Завершённые заказы</h2>
-      {completedOrders.length > 0 ? (
-        <ul className={styles.table}>
-          <div className={styles.tableHeader}>
-            <h3>ID заказа</h3>
-            <h3>Пользователь</h3>
-            <h3>Список товаров</h3>
-            <h3>Дата оформления</h3>
-            <h3>Сумма</h3>
-          </div>
-          {completedOrders.map(adminOrder => (
-            <AdminOrderItem key={adminOrder.id} adminOrder={adminOrder} />
-          ))}
-        </ul>
-      ) : (
-        <>
-          <h2 className={styles.emptyCartTitle}>Нет записей</h2>
-        </>
-      )}
+      <div className={styles.tableContainer}>
+        <h2 className={styles.title}>Завершённые заказы</h2>
+        {completedOrders.length > 0 ? (
+          <ul className={styles.table}>
+            <div className={styles.tableHeader}>
+              <h3>ID заказа</h3>
+              <h3>Пользователь</h3>
+              <h3>Список товаров</h3>
+              <h3>Дата оформления</h3>
+              <h3>Сумма</h3>
+            </div>
+            {completedOrders.map(adminOrder => (
+              <AdminOrderItem key={adminOrder.id} adminOrder={adminOrder} />
+            ))}
+          </ul>
+        ) : (
+          <>
+            <h2 className={styles.emptyCartTitle}>Нет записей</h2>
+          </>
+        )}
+      </div>
     </>
   );
 }
