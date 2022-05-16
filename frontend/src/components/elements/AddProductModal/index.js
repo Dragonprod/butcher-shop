8f9ac046bdf3ab8a67b5d847cc182d779d3ea16b
maxcore25 <mkorobov2002@mail.ru>
2022-05-17 01:14:36 +0300

    AddProductModal Created

---
+++ frontend/src/components/elements/AddProductModal/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Modal from '../Modal';
+import styles from './AddCategoryModal.module.scss';
+
+export default function AddProductModal() {
+  return <Modal>AddProductModal</Modal>;
+}
