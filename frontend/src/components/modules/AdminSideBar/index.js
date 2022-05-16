156eba25885ad33d71945bb00be8f129717c7919
maxcore25 <mkorobov2002@mail.ru>
2022-05-16 15:44:13 +0300

    Admin Links Added And Stylized

--- frontend/src/components/modules/AdminSideBar/index.js
+++ frontend/src/components/modules/AdminSideBar/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import styles from './AdminSideBar.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function AdminSideBar() {
-  return <aside className={styles.aside}>AdminSideBar</aside>;
+  return (
+    <aside className={styles.aside}>
+      <nav className={styles.menu}>
+        <Link to='/admin'>Пользователи</Link>
+        <Link to='/admin/orders'>Заказы</Link>
+        <Link to='/admin/products'>Продукты</Link>
+      </nav>
+    </aside>
+  );
 }
