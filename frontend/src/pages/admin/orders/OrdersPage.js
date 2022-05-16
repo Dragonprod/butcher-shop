92019ecb2a0316e75dbe081bb2e44328ce886350
maxcore25 <mkorobov2002@mail.ru>
2022-05-16 17:32:18 +0300

    OrdersPage JSX Added

--- frontend/src/pages/admin/orders/OrdersPage.js
+++ frontend/src/pages/admin/orders/OrdersPage.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import AdminSideBar from '../../../components/modules/AdminSideBar';
+import Header from '../../../components/modules/Header';
 import styles from './OrdersPage.module.scss';
 
 export default function OrdersPage() {
-  return <div>OrdersPage</div>;
+  return (
+    <>
+      <Header />
+      <section className={styles.mainContainer}>
+        <AdminSideBar />
+      </section>
+    </>
+  );
 }
