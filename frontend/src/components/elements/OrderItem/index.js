edd4e8b63b132c4fd27ede5e6029e61374f66fdc
maxcore25 <mkorobov2002@mail.ru>
2022-05-16 11:30:10 +0300

    OrderItem Stylized

--- frontend/src/components/elements/OrderItem/index.js
+++ frontend/src/components/elements/OrderItem/index.js
@@ -1,7 +1,24 @@
 import styles from './OrderItem.module.scss';
 
-const OrderItem = () => {
-  return <div className={styles.itemContainer}></div>;
+const OrderItem = ({ cartItem: orderItem }) => {
+  return (
+    <li className={styles.itemContainer}>
+      <div className={styles.photoContainer}>
+        <img src={orderItem.photoUrl} alt={orderItem.name} />
+      </div>
+      <div className={styles.contentContainer}>
+        <h3>{orderItem.name}</h3>
+        <p>{orderItem.weight} кг</p>
+      </div>
+      <div className={styles.priceContainer}>
+        <h4>{orderItem.price} ₽</h4>
+      </div>
+      <div className={styles.counterContainer}></div>
+      <div className={styles.sumContainer}>
+        <h4>{999} ₽</h4>
+      </div>
+    </li>
+  );
 };
 
 export default OrderItem;
