652e7d4ad043eb3889c51137fed224412f164915
maxcore25 <mkorobov2002@mail.ru>
2022-05-16 13:18:05 +0300

    AdminPage Background Stylized

--- frontend/src/pages/admin/AdminPage.js
+++ frontend/src/pages/admin/AdminPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../../components/modules/Footer';
 import Header from '../../components/modules/Header';
 import styles from './AdminPage.module.scss';
 
@@ -6,7 +7,8 @@ export default function AdminPage() {
   return (
     <>
       <Header />
-      <section className={styles.mainGrid}></section>
+      <section className={styles.mainContainer}></section>
+      <Footer />
     </>
   );
 }
