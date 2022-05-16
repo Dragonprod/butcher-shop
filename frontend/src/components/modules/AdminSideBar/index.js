d7a81cb668be93cee1e79ecb792f569a159a580d
maxcore25 <mkorobov2002@mail.ru>
2022-05-16 15:27:19 +0300

    AdminSideBar Created

---
+++ frontend/src/components/modules/AdminSideBar/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './AdminSideBar.module.scss';
+
+export default function AdminSideBar() {
+  return <aside className={styles.aside}>AdminSideBar</aside>;
+}
